a0fdd29965edcd352f5c6ca902701f62886b4395
Ruslan <53014973+malsagov@users.noreply.github.com>
2021-03-13 13:42:23 +0700

    Added extension for redux

--- src/redux/users-ruducer.js
+++ src/redux/users-ruducer.js
@@ -93,40 +93,35 @@ export const toggleFollowingProgress = (isFetching, userId) => ({type: TOGGLE_IS
 
 export const requestUsers = (page, pageSize) => {
 
-    return (dispatch) => {
+    return async (dispatch) => {
         dispatch(toggleIsFetching(true))
 
-        usersAPI.getUsers(page, pageSize).then(data => {
-        return(
-            dispatch(toggleIsFetching(false)),
-            dispatch(setUsers(data.items)),
-            dispatch(setTotalUsersCount(data.totalCount))
-        )   
-        })
+        let data = await usersAPI.getUsers(page, pageSize)
+        dispatch(toggleIsFetching(false))
+        dispatch(setUsers(data.items))
+        dispatch(setTotalUsersCount(data.totalCount))
     }
 }
 export const follow = (userId) => {
 
-    return (dispatch) => {
+    return async (dispatch) => {
         dispatch(toggleFollowingProgress(true, userId))
-        usersAPI.follow(userId).then(data => {
-                if (data.resultCode === 0) {
-                    dispatch(followSuccess(userId))
-                    dispatch(toggleFollowingProgress(false, userId))
-                }
-            })
+        let data = await usersAPI.follow(userId)
+        if (data.resultCode === 0) {
+            dispatch(followSuccess(userId))
+            dispatch(toggleFollowingProgress(false, userId))
+        }
     }
 }
 export const unfollow = (userId) => {
 
-    return (dispatch) => {
+    return async (dispatch) => {
         dispatch(toggleFollowingProgress(true, userId))
-        usersAPI.unfollow(userId).then(data => {
-                if (data.resultCode === 0) {
-                    dispatch(unfollowSuccess(userId))
-                    dispatch(toggleFollowingProgress(false, userId))
-                }
-            })
+        let data = await usersAPI.unfollow(userId)
+        if (data.resultCode === 0) {
+            dispatch(unfollowSuccess(userId))
+            dispatch(toggleFollowingProgress(false, userId))
+        }
     }
 }
 
